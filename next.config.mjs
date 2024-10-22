58eb89245f005d618836d9da0f112a6b5db728b4
Franco Anfossi <franco.anfossi@uc.cl>
2024-10-22 12:33:17 -0300

    feat(config): add remote image patterns for Gravatar and Auth0

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 's.gravatar.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'cdn.auth0.com',
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
